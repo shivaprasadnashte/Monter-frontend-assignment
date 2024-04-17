107f6c1c463e57294f019ecf5d8aa09610ddd399
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-04-18 02:50:34 +0530

    Refactor report pagination logic

--- app/page.js
+++ app/page.js
@@ -1,157 +1,117 @@
-"use client";
+"use client"
 import { CiFilter } from "react-icons/ci";
 import { RxCross2 } from "react-icons/rx";
 import { TbFileDownload } from "react-icons/tb";
-import { MdOutlineSkipPrevious } from "react-icons/md";
-import { MdOutlineSkipNext } from "react-icons/md";
-import { useState } from "react";
+import { MdOutlineSkipPrevious, MdOutlineSkipNext } from "react-icons/md";
+import { useState, useEffect } from "react";
+import report from "../public/data";
 
 export default function Home() {
-  const report = [
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-    {
-      date: "27 07 2021",
-      time: "16 02 PM",
-      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
-    },
-  ];
-  const [noOfRows, setNoOfRows] = useState(1);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState();
+  const [noOfRows, setNoOfRows] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [displayedReport, setDisplayedReport] = useState([]);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * noOfRows;
+    const endIndex = Math.min(startIndex + parseInt(noOfRows), report.length);
+    console.log(endIndex)
+    setDisplayedReport(report.slice(startIndex, endIndex));
+  }, [noOfRows, currentPage]);
+
+
+
+
+  const totalPages = Math.ceil(report.length / noOfRows);
+
   const handleNext = () => {
-    setStart(start + noOfRows);
-    setEnd(start + noOfRows);
-   
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
   };
+
   const handlePrev = () => {
-    setStart(start - noOfRows);
-    setEnd(start - noOfRows);
-   
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   return (
     <>
       <div className="flex flex-col items-center sm:p-10 shadow-2xl m-1 sm:my-5 sm:mx-5 min-h-screen">
-        <div className=" flex justify-center  w-full ">
-          <div className=" flex justify-evenly w-full">
-            <div className=" w-3/4 flex sm:justify-center ">
-              <h1 className=" font-bold text-sm  sm:text-xl">
+        <div className="flex justify-center w-full">
+          <div className="flex justify-evenly w-full">
+            <div className="w-3/4 flex sm:justify-center">
+              <h1 className="font-bold text-sm sm:text-xl">
                 Recently Generated Report
               </h1>
             </div>
-            <div className=" flex gap-4 w-1/6 justify-end ">
-              <div className=" border-[1px]  border-black rounded-md h-6 sm:w-8 sm:h-8 flex justify-center items-center">
+            <div className="flex gap-4 w-1/6 justify-end">
+              <div className="border-[1px] border-black rounded-md h-6 sm:w-8 sm:h-8 flex justify-center items-center">
                 <CiFilter className="sm:text-2xl" />
               </div>
-              <div className=" border-[1px]  border-black rounded-md sm:w-8 h-6 sm:h-8 flex justify-center items-center">
+              <div className="border-[1px] border-black rounded-md sm:w-8 h-6 sm:h-8 flex justify-center items-center">
                 <RxCross2 className="sm:text-2xl" />
               </div>
             </div>
           </div>
         </div>
-        <div className=" flex w-full justify-evenly bg-gray-100 p-2">
-          <div className=" flex text-xs font-bold justify-start w-full sm:w-1/4">
+        <div className="flex w-full justify-evenly bg-gray-100 p-2">
+          <div className="flex text-xs font-bold justify-start w-full sm:w-1/4">
             Date
           </div>
-          <div className=" flex text-xs font-bold justify-start w-full">
+          <div className="flex text-xs font-bold justify-start w-full">
             Report Name
           </div>
-          <div className=" flex text-xs font-bold justify-start w-full sm:w-1/4">
+          <div className="flex text-xs font-bold justify-start w-full sm:w-1/4">
             Download
           </div>
         </div>
-        {report.slice(start, end).map((item, index) => (
+        {displayedReport.map((item, index) => (
           <div
             key={index}
-            className=" flex w-full justify-evenly p-2 border-t border-gray-200"
+            className="flex w-full justify-evenly p-2 border-t border-gray-200"
           >
-            <div className="  text-xs justify-start w-full sm:w-1/4">
+            <div className="text-xs justify-start w-full sm:w-1/4">
               <div>{item.date}</div>
               <div>{item.time}</div>
             </div>
-            <div className=" flex text-xs justify-start w-full">
+            <div className="flex text-xs justify-start w-full">
               {item.name}
             </div>
-            <div className=" flex text-xs justify-start w-full sm:w-1/4">
-              <div className=" border-[1px]  border-black rounded-md h-6 w-8 flex justify-center items-center">
+            <div className="flex text-xs justify-start w-full sm:w-1/4">
+              <div className="border-[1px] border-black rounded-md h-6 w-8 flex justify-center items-center">
                 <TbFileDownload className="sm:text-2xl" />
               </div>
             </div>
           </div>
         ))}
-        <div className=" flex w-full items-center justify-center gap-10">
+        <div className="flex w-full items-center justify-center gap-10">
           <div>
-            <div className=" flex justify-center items-center gap-4">
-              <div className=" flex gap-1">
-                <MdOutlineSkipPrevious className="sm:text-2xl" onClick={handlePrev} />
+            <div className="flex justify-center items-center gap-4">
+              <div className="flex gap-1">
+                <MdOutlineSkipPrevious
+                  className="sm:text-2xl cursor-pointer"
+                  onClick={handlePrev}
+                />
                 <p>Prev</p>
               </div>
               <div className="border-2 flex justify-center items-center bg-orange-500 w-8 h-8">
-                <p>1</p>
+                <p>{currentPage}</p>
               </div>
-
-              <div className=" flex gap-1" onClick={handleNext}>
+              <div className="flex gap-1 cursor-pointer" onClick={handleNext}>
                 <p>Next</p>
                 <MdOutlineSkipNext className="sm:text-2xl" />
               </div>
             </div>
           </div>
-
-          <div className=" flex gap-2">
+          <div className="flex gap-2">
             Rows per page
             <input
               type="number"
               placeholder="10"
-              className=" focus:outline-none w-12 h-6 border-2 border-gray-500 px-1"
+              className="focus:outline-none w-12 h-6 border-2 border-gray-500 px-1"
               value={noOfRows}
               onChange={(e) => setNoOfRows(e.target.value)}
-              onClick={() => {
-                setEnd(start + noOfRows);
-              }}
             />
           </div>
         </div>
---
+++ public/data.js
@@ -0,0 +1,196 @@
+const report = [
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },{
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+    {
+      date: "27 07 2021",
+      time: "16 02 PM",
+      name: "Lorem ipsummkrl; edne; l3dne; 3dnej; 3ndje n3c; dolor sit amet",
+    },
+  ];
+
+    export default report;
